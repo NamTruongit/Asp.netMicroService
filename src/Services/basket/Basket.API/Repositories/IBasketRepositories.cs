e2fca75b0adf35f4c1c0c59c51872717d570090b
namdev <namtruong.it.dn@gmail.com>
2022-10-28 19:39:45 +0700

    add basket Repository to project

---
+++ src/Services/basket/Basket.API/Repositories/IBasketRepositories.cs
@@ -0,0 +1,12 @@
+﻿using Basket.API.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepositories
+    {
+        Task<ShoppingCart> GetBasket(string username);
+        Task<ShoppingCart> Updatebasket(ShoppingCart cart);
+        Task DeleteBasket(string username);
+    }
+}
