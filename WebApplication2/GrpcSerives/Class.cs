da9efb72e4e1a91acd0a90cbddf85836ba76e1f2
namdev <namtruong.it.dn@gmail.com>
2022-11-03 14:51:49 +0700

    Add ordering serivce to projet

--- WebApplication2/GrpcSerives/Class.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace WebApplication2.GrpcSerives
-{
-    public class Class
-    {
-    }
-}
---
+++ src/Services/Ordering/Ordering.Doman/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Ordering.Doman
+{
+    public class Class1
+    {
+    }
+}
