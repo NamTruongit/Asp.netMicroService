e00964656e92db8da582dffd3b2d3eeb2e0ae2dc
namdev <namtruong.it.dn@gmail.com>
2022-11-07 11:53:51 +0700

    Implement part Infrastructure

---
+++ src/Services/Ordering/Ordering.Infrastructure/Mail/EmailService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Model;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Mail
+{
+    public class EmailService : IEmailService
+    {
+        public EmailSetting _emailSetting { get; }
+        public ILogger<EmailService> _logger { get; }
+        public EmailService(IOptions<EmailSetting> emailSettings,ILogger<EmailService> logger)
+        {
+            _emailSetting = emailSettings.Value;
+            _logger = logger;
+        }
+        public async Task<bool> SendMail(Email email)
+        {
+            var client = new SendGridClient(_emailSetting.ApiKey);
+            var subject = email.Subject;
+            var to = new EmailAddress(email.To);
+            var emailBody = email.Body;
+            var from = new EmailAddress
+            {
+                Email = _emailSetting.FromAddress,
+                Name = _emailSetting.FromName
+            };
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from,to,subject,emailBody,emailBody);
+            var reponse = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Mail sent");
+
+            if (reponse.StatusCode  == System.Net.HttpStatusCode.Accepted|| reponse.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            _logger.LogError("Email Sending failed");
+            return false;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistance/OrderContextSeed.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Doman.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistance
+{
+    public class OrderContextSeed
+    {
+        public static async  Task SeeAsync(OrderContext orderContext,ILogger<OrderContextSeed> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                orderContext.Orders.AddRange(GetPreConfiguredOrders());
+                await orderContext.SaveChangesAsync();
+                logger.LogDebug("See database associated with context {DbContextName} ",typeof(OrderContext).Name);
+            }
+        }
+        private static IEnumerable<Order> GetPreConfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() { UserName= "namAdm",FirstName ="nam",LastName="truong",EmailAddress="namtruong.it.dn@gmail.com",AddressLine="Da nang",Country = "Viet Nam",TotalPrice=500 }
+            };
+        }
+    }
+}
